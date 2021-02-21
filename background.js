971f5a17e035dae912a345e0ab64b80a2a681f66
maknarfanar <78266567+maknarfanar@users.noreply.github.com>
2021-02-21 17:33:57 -0500

    Update background.js

--- background.js
+++ background.js
@@ -4,39 +4,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
    console.log("Copying");
 });																																																																																																																																																																																																																																																																																	
 
-
-	
-
-
-
-/*
-// create element for pasting
-const textEl = document.createElement('textarea');
-document.body.appendChild(textEl);
-
-var prevPasted = '';
-setInterval(function () {
-    // paste text from clipboard to focused textarea
-    textEl.focus();
-    textEl.value = '';
-    document.execCommand('paste');
-    const pastedText = textEl.value;
-
-    // simple cache check
-    if (pastedText !== prevPasted) {
-        prevPasted = pastedText;
-
-        if (pastedText.match(/https?:/)) { // you can improve you URL scheme
-
-            // get the current tab
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                var tab = tabs[0];
-
-                // check if current tab has the same URL
-                if (tab.url === pastedText) {
-                    console.log('Omnibox URL:', pastedText);
-                }
-            });
-        }
-    }
-}, 500);*/
